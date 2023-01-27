7f9e81c30e7e5ae3093a6647ca7b6e8d26237fca
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-01-27 16:44:43 +0530

    optional args

---
+++ optionals.ts
@@ -0,0 +1,43 @@
+const printIngredients = (quantity:string, Ingredient:string, extra?:string)=>{
+console.log(`${quantity} ${Ingredient} ${extra?`${extra}`:""}`);
+}
+
+printIngredients("120","basil")
+printIngredients("1mg","sugar", "Optional")
+
+interface User{
+    name:string,
+    info?:{
+        email?:string
+    }
+}
+
+
+
+function findEmail(user:User):string{
+    if(user.info){
+        return user.info.email as string;
+    } 
+    return ""
+}
+// or
+function findEmailnew(user:User):string{
+    if(user.info){
+        return user.info.email!;
+    } 
+    return ""
+}
+// or
+function findEmailEasy(user:User):string{
+    if(user.info){
+        return user?.info?.email ?? "";
+    } 
+    return ""
+}
+
+console.log(findEmail({name:"vishal Gupta", info:{email:"vishal"}}))
+console.log(findEmailnew({name:"vishal Gupta", info:{email:"vishal"}}))
+
+console.log(findEmailEasy({name:"vishal Gupta", info:{email:"vishal"}}))
+
+// optional Callbacks
--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -11,29 +11,29 @@ interface coordinate{
 //     return {x,y}
 // }
 
-function parseCoordinate(obj:coordinate):coordinate;
-function parseCoordinate(x:number, y:number):coordinate;
-function parseCoordinate(str:string):coordinate;
+// function parseCoordinate(obj:coordinate):coordinate;
+// function parseCoordinate(x:number, y:number):coordinate;
+// function parseCoordinate(str:string):coordinate;
 
-function parseCoordinate(arg1:unknown, arg2?:unknown):coordinate{
-let coord:coordinate = {x:0,y:0}
-if(typeof arg1 === 'object'){
-    coord =  {...arg1 as coordinate }
-}
-else if(typeof arg1=== 'string'){
- (arg1 as string).split(",").forEach((str)=>{
-    const [key,val] = str.trim().split(":")
-    coord[key as "x"|"y"] = parseInt(val)
- })
-}
-else{
-    coord = {x:arg1 as number,y:arg2 as number}}
-    return coord
-}
+// function parseCoordinate(arg1:unknown, arg2?:unknown):coordinate{
+// let coord:coordinate = {x:0,y:0}
+// if(typeof arg1 === 'object'){
+//     coord =  {...arg1 as coordinate }
+// }
+// else if(typeof arg1=== 'string'){
+//  (arg1 as string).split(",").forEach((str)=>{
+//     const [key,val] = str.trim().split(":")
+//     coord[key as "x"|"y"] = parseInt(val)
+//  })
+// }
+// else{
+//     coord = {x:arg1 as number,y:arg2 as number}}
+//     return coord
+// }
 
-console.log(parseCoordinate({x:23,y:45}))
-console.log(parseCoordinate(24,67))
-console.log(parseCoordinate("x:12, y:32"))
+// console.log(parseCoordinate({x:23,y:45}))
+// console.log(parseCoordinate(24,67))
+// console.log(parseCoordinate("x:12, y:32"))
 
 // const strr = "x:12, y:32";
 // let cord:coordinate
@@ -43,4 +43,41 @@ console.log(parseCoordinate("x:12, y:32"))
 // })
 
 // console.log(stra)
+
+function parseCoordinates(obj:coordinate):coordinate;
+function parseCoordinates(x:number, y:number):coordinate;
+function parseCoordinates(arg1:string):coordinate;
+
+function parseCoordinates(arg1:unknown, arg2?:unknown):coordinate{
+    let coord:coordinate = {
+        x:0,
+        y:0
+    }
+
+    if(typeof arg1 === "object"){
+        coord = {
+            ...arg1 as coordinate
+        }
+    }
+    else if(typeof arg1 === "string"){
+        (arg1 as string).split(",").forEach(str=>{const [key,value]=str.split(":")
+    coord[key as "x"|"y"] = parseInt(value,10)})
+    }
+    
+    else{
+        coord = {
+            x:arg1 as number,
+            y:arg2 as number
+        }
+    }
+    return coord
+}
+
+console.log(parseCoordinates({x:12,y:34}))
+console.log(parseCoordinates(44,98))
+console.log(parseCoordinates("x:44,y:55"))
+
+
+
+
 
