228f83a4e9f6c5cea83122f599338dfc16987f9f
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-01-27 14:38:44 +0530

    coordinate as string

--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -13,10 +13,18 @@ interface coordinate{
 
 function parseCoordinate(obj:coordinate):coordinate;
 function parseCoordinate(x:number, y:number):coordinate;
+function parseCoordinate(str:string):coordinate;
+
 function parseCoordinate(arg1:unknown, arg2?:unknown):coordinate{
 let coord:coordinate = {x:0,y:0}
 if(typeof arg1 === 'object'){
     coord =  {...arg1 as coordinate }
+}
+else if(typeof arg1=== 'string'){
+ (arg1 as string).split(",").forEach((str)=>{
+    const [key,val] = str.trim().split(":")
+    coord[key as "x"|"y"] = parseInt(val)
+ })
 }
 else{
     coord = {x:arg1 as number,y:arg2 as number}}
@@ -25,3 +33,14 @@ else{
 
 console.log(parseCoordinate({x:23,y:45}))
 console.log(parseCoordinate(24,67))
+console.log(parseCoordinate("x:12, y:32"))
+
+// const strr = "x:12, y:32";
+// let cord:coordinate
+// const stra = strr.split(",").forEach(str =>
+//     { const [key,value] = str.split(":");
+//     cord[key as "x"|"y"] = parseInt(value,10)
+// })
+
+// console.log(stra)
+
