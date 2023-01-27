8a7d8ebe28c49b211afcac64e7d4d16c50c24944
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-01-27 12:36:38 +0530

    callback functions

---
+++ funcs-and-funcs.ts
@@ -0,0 +1,21 @@
+export function printToFile(text: string,callback:()=>void):void{
+    console.log(text)
+    callback();
+}
+
+type mutationFunction = (v:number)=>number
+
+export const mutateArray = (numbers:number[], mutate:mutationFunction):number[]=>{
+return numbers.map(mutate)
+}
+
+console.log(mutateArray([1,2,3], (v)=>v*10))
+
+
+export const createAdder = (v:number):(val: number) => number=>{
+    return (val:number)=>val+v
+}
+
+const adder = createAdder(55)
+
+console.log(adder(1))
