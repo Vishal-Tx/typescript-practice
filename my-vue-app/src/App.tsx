a742a2b45d9c7f18572aea42e78b10b47a14ef07
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-01-31 19:03:39 +0530

    list done

--- my-vue-app/src/App.tsx
+++ my-vue-app/src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { PropsWithChildren, useCallback } from 'react'
 import "./App.css"
 
 const Heading = ({title}:{title:string})=>{
   return <h1>{title}</h1>
 }
 
-const Box = ({children}:{children:React.ReactNode})=>{
+const Box:React.FC<PropsWithChildren>= ({children})=>{
 return <div style={{color:"brown"}}>{children}</div>
 }
 
+const List:React.FC<{items:string[],onClick?:(item:string)=>void}>= ({items, onClick})=>(
+  <ul>
+    {
+      items.map((item,index)=><li key={index} onClick={()=>onClick?.(item)}>{item}</li>
+      )
+    }
+  </ul>
+)
+
 const App = () => {
+  const onListClick = useCallback((item:string)=>{
+    alert(item)
+  },[])
   return (
     <div><Heading title = "Hello"/>
-    <Box>Hello there</Box></div>
+    <Box>Hello there</Box>
+    <List items={["1","2","3"]} onClick={onListClick}/></div>
     
   )
 }
