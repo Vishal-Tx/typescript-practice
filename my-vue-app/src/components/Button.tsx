2e05244727b612937867c3d98451d1d97095197c
Vishal-TX <vishalguptaa2z111@gmail.com>
2023-02-10 18:08:58 +0530

    comment added

--- my-vue-app/src/components/Button.tsx
+++ my-vue-app/src/components/Button.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Button:React.FC<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>&{title?:string}> = ({children, title, style, ...rest}) => {
+import React from "react";
+//button Add
+const Button: React.FC<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & { title?: string }
+> = ({ children, title, style, ...rest }) => {
   return (
-    <button {...rest} style={{...style, backgroundColor:"brown"}}>{title ?? children}</button>
-  )
-}
+    <button {...rest} style={{ ...style, backgroundColor: "brown" }}>
+      {title ?? children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
